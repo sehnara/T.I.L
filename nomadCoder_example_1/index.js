bdd98544c61dfd459565b551a866552d520b9543
sehoon <gsh723@naver.com>
2021-04-28 15:38:13 +0900

    - youtube 'nomad coder' javascript 예제 1(random number game)
    3) javascript click event 만들기

--- nomadCoder_example_1/index.js
+++ nomadCoder_example_1/index.js
@@ -2,16 +2,54 @@ const textRange = document.querySelector('.js-textRange');
 
 const form = document.querySelector('.js-formRange');
 const numInput = form.querySelector('input');
+const choose = document.querySelector('.js-chooseNum');
+const userInput = choose.querySelector('.js-userInput');
+const submit = choose.querySelector('.js-submit');
 
-let maxvalue = 0;
+const result = document.querySelector('.js-result');
 
+let maxValue = 0;
+
+function handleClick(){ 
+  // 결과를 보여줄 paragraph 2줄 생성
+  const para1 = document.createElement('p');
+  const para2 = document.createElement('p');  
+
+  // get userNum & machineNum 
+  let randNum = getRandomNum();
+  let userNum = parseInt(userInput.value);
+
+  // 결과 paragraph에 결과 문장 넣어주기
+  para1.innerHTML = `You Chose : ${userNum}, the machine chose : ${randNum}`;
+
+  if(randNum === userNum){
+    para2.innerHTML = `You win!`;
+  }else{
+    para2.innerHTML = `You lost!`;
+  } 
+  result.appendChild(para1);
+  result.appendChild(para2);
+}
+
+function chooseNum(){
+  submit.addEventListener('click',handleClick);
+}
+
+function getRandomNum(){
+  const randomNum = Math.floor(Math.random()*(maxValue+1));  
+  return randomNum;
+}
 function handleChange(){
-  maxvalue = numInput.value;  
-  textRange.innerHTML = `Generate a number between 0 and ${maxvalue}`;  
+  maxValue = parseInt(numInput.value);  
+  textRange.innerHTML = `Generate a number between 0 and ${maxValue}`;  
 }
 
 function init(){
+
   // 1. input(range) 변화 값을 h2에 반영
   numInput.addEventListener('change', handleChange);
+  // 2. user와 machine 모두 숫자 선택
+  chooseNum();
+
 }
 init();
