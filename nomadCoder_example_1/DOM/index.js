24ceef976e4d6d276c3f9b55762111af28bab4e1
sehoon <gsh723@naver.com>
2021-05-03 16:47:55 +0900

    - DOM object method(mouseover,mouseout)
    1) mouseover 이벤트 구현
    2) mouseout 이벤트 구현
    3) mouse hover시 이미지가 바뀌는 기능 구혀

--- nomadCoder_example_1/DOM/index.js
+++ nomadCoder_example_1/DOM/index.js
@@ -10,7 +10,8 @@ const img3 = d3.querySelector('img');
 const d4 = document.querySelector('.d4');
 const img4 = d4.querySelector('img');
 
-
+const mouse = document.querySelector('.mouse');
+const mouse_img = mouse.querySelector('img');
 
 function handleClick(event){  
   const img = event.target.getAttribute('src');
@@ -21,5 +22,15 @@ d2.addEventListener('click',handleClick);
 d3.addEventListener('click',handleClick);
 d4.addEventListener('click',handleClick);
 
+function handleMouseOver(){
+  const img = 'b.png';
+  mouse_img.setAttribute('src',img);
+}
 
+function handleMouseOut(){
+  const img = 'a.png';
+  mouse_img.setAttribute('src',img);
+}
 
+mouse_img.addEventListener('mouseover',handleMouseOver);
+mouse_img.addEventListener('mouseout',handleMouseOut);
